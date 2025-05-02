95ccdd11b5ab9a3ec42df5a3e507a43201dcabcc
JoeMolinelli <116307221+TheGoldDiamond9@users.noreply.github.com>
2025-05-02 19:00:58 -0400

    Update config.js

--- webroot/js/config.js
+++ webroot/js/config.js
@@ -29,7 +29,7 @@ var audioSettings = {
   shuffle: false,
   randomStart: true,
   narrations: true,
-  order: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, /*16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39*/], //1 to 39
+  order: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], //1 to 15
 };
 var introPackage = {group: "intro", slides: [
   { function: "introSlide" },
